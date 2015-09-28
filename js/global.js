983c32efb7e9afe07d2127478ba9d8e7e1a69d3c
Matt Litzinger <mattl@browndesign.com>
2015-09-28 15:18:02 -0400

    Added target:blank to generated link

--- js/global.js
+++ js/global.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
 			};
 
 			$.post('http://api.litzo.us/links', data, function(msg) {
-			  $('.title').html('<a href="http://litzo.us/' + msg.code + '"><span>litzo.us</span>/' + msg.code + '</a>');
+			  $('.title').html('<a href="http://litzo.us/' + msg.code + '" target="_blank"><span>litzo.us</span>/' + msg.code + '</a>');
 			});
 
 		} else {
